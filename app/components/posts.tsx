f618e5416d5655a19fbccd20c93bcf625a884e03
Leticia @levxyca <leticiacaroline-123@hotmail.com>
2024-10-16 14:42:37 +0000

    :bug: sort recent blog posts

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import Link from 'next/link';
+import { formatDate, getBlogPosts } from 'app/blog/utils';
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+  let allBlogs = getBlogPosts();
 
   return (
     <div>
       {allBlogs
-        .sort((a, b) => new Date(b.metadata.publishedAt) - new Date(a.metadata.publishedAt)) // Ordena do mais recente para o mais antigo
+        .sort((a, b) => {
+          const dateA = a.metadata.publishedAt ? new Date(a.metadata.publishedAt) : new Date(0); // data mínima
+          const dateB = b.metadata.publishedAt ? new Date(b.metadata.publishedAt) : new Date(0); // data mínima
+          
+          return dateB.getTime() - dateA.getTime(); // Ordena do mais recente para o mais antigo
+        })
         .slice(0, 3) // Pega apenas os três primeiros posts
         .map((post) => (
           <Link
@@ -26,5 +31,5 @@ export function BlogPosts() {
           </Link>
         ))}
     </div>
-  )
+  );
 }
